46ad60e4c40cde1371746e0d191a6258f16ebbc2
RajParmar03 <rjbusiness03@gmain.com>
2022-10-02 13:41:27 +0530

    added files of day2.

---
+++ chargebee/src/Context/AppContextProvider.jsx
@@ -0,0 +1,18 @@
+import { createContext, useReducer } from "react";
+import { reducer } from "../Reducer/reducer";
+import { store } from "../Reducer/store";
+
+
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children}) => {
+
+    const [state, dispatch] = useReducer(reducer, store)
+
+    return (
+        <AppContext.Provider value={{state,dispatch}}>
+            {children}
+        </AppContext.Provider>
+    )
+}
